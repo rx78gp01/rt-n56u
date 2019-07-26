a34dd93ef64a3fa9e706e61070019a1c60760470
google <android@google.com>
2019-07-26 11:22:27 -0700

    Revert "kernel-3.x.x: udp: only allow UFO for packets from SOCK_DGRAM sockets, commit acf8dd0a upstream"
    This reverts commit 689a910cf6d6a70f3f2e428e503fa0aa593963b2.

--- trunk/linux-3.4.x/net/ipv4/ip_output.c
+++ trunk/linux-3.4.x/net/ipv4/ip_output.c
@@ -844,8 +844,7 @@ static int __ip_append_data(struct sock *sk,
 	cork->length += length;
 	if (((length > mtu) || (skb && skb_has_frags(skb))) &&
 	    (sk->sk_protocol == IPPROTO_UDP) &&
-	    (rt->dst.dev->features & NETIF_F_UFO) && !rt->dst.header_len &&
-	    (sk->sk_type == SOCK_DGRAM)) {
+	    (rt->dst.dev->features & NETIF_F_UFO) && !rt->dst.header_len) {
 		err = ip_ufo_append_data(sk, queue, getfrag, from, length,
 					 hh_len, fragheaderlen, transhdrlen,
 					 maxfraglen, flags);
